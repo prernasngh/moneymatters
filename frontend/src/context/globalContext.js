77c01987d480dfdbb0b6b1aafeaa86f4ed5a65b7
Prerna Singh <121041518+prernasngh@users.noreply.github.com>
2023-07-15 03:04:04 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 // const baseURL = "http://localhost:5000/api/v1/";
 // const baseURL="https://moneymatters-node-m1ffevn88-prernasngh.vercel.app/api/v1/"
-// const baseURL="https://moneymatters-node.vercel.app/api/v1";
-const baseURL="https://moneymatters-node-hgyo2qunx-prernasngh.vercel.app/api/v1/";
+const baseURL="https://moneymatters-node.vercel.app/api/v1/";
+// const baseURL="https://moneymatters-node-hgyo2qunx-prernasngh.vercel.app/api/v1/";
 
 const GlobalContext = React.createContext();
 
